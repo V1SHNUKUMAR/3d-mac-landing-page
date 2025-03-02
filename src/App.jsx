32d2bde2b0955ac933ed0a0dbf904cabbf01a5ef
Vishnu kumar <work.vishnukumar@gmail.com>
2025-03-02 17:28:41 +0530

    changed for mnobile

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div className="h-screen w-full">
-      <div className="navbar flex items-center gap-10 absolute z-10 top-7 left-1/2 -translate-x-1/2">
+      <div className="hidden md:flex navbar items-center gap-10 absolute z-10 top-7 left-1/2 -translate-x-1/2">
         {navMenu.map((menuItem) => (
           <button
             key={menuItem}
@@ -31,27 +31,33 @@ function App() {
           </button>
         ))}
       </div>
-      <div className="flex flex-col gap-2 items-center absolute top-[116px] left-1/2 -translate-x-1/2 text-white font-['Helvetica']">
-        <h3 className="masked text-7xl tracking-tighter font-bold">
+      <button className="md:hidden space-y-2.5 absolute top-6 left-6">
+        <div className="w-[30px] h-[1px] bg-white"></div>
+        <div className="w-[30px] h-[1px] bg-white"></div>
+      </button>
+      <div className="flex flex-col gap-2 items-center text-center absolute top-[116px] left-1/2 -translate-x-1/2 text-white font-['Helvetica']">
+        <h3 className="masked w-full whitespace-nowrap text-4xl md:text-7xl tracking-tighter font-bold">
           macbook pro.
         </h3>
-        <h5>Oh so pro !</h5>
-        <p className="text-center text-sm  w-3/4">
+        <h5 className="text-xs md:text-base">Oh so pro !</h5>
+        <p className="text-center text-xs md:text-sm  w-full">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi aperiam
           magnam natus ipsa.
         </p>
       </div>
-      <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
-        {/* <OrbitControls /> */}
-        <Environment
-          files={[
-            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
-          ]}
-        />
-        <ScrollControls pages={1}>
-          <MacContainer />
-        </ScrollControls>
-      </Canvas>
+      <div className="relative top-[30%] w-full h-[50vh] sm:h-[60vh] md:h-[90vh]">
+        <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+          {/* <OrbitControls /> */}
+          <Environment
+            files={[
+              "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+            ]}
+          />
+          <ScrollControls pages={1}>
+            <MacContainer />
+          </ScrollControls>
+        </Canvas>
+      </div>
     </div>
   );
 }
